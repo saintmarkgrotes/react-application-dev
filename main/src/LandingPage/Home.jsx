07dd787b0d69589744a793222ed61dc1eaf0fa10
unknown <grotessaintmark@gmail.com>
2026-03-31 16:05:23 +0800

    landing page edit

---
+++ main/src/LandingPage/Home.jsx
@@ -0,0 +1,22 @@
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <main class="container">
+            <h1>React App</h1>
+            <p>This is a simple landing page, where you can see the some of the information and features of our website.</p>
+            <section>
+                <p>Here are our members:</p>
+
+                <ul>
+                    <li>Grotes, Saint Mark D.</li>
+                    <li>Mediante, Arvin Clark G.</li>
+                    <li>Ostulano, Janna M.</li>
+                    <li>Versaga, Jullever D.</li>
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+export default Home;
