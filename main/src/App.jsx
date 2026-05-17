e121c99d7047ff81bad02bc0dd6603fc6827b3e8
unknown <grotessaintmark@gmail.com>
2026-05-17 08:15:14 +0800

    activity 3 commit

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/activities/activity-1/Home";
 import Activity2 from "./pages/activities/activity-2/Activity2";
+import Activity3 from "./pages/activities/activity-3/Activity3";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/home" element={<Home />} />
-                <Route path="/" element={<Activity2 />} />
+                <Route path="/login" element={<Activity2 />} />
+                <Route path="/" element={<Activity3 />} />
             </Routes>
         </BrowserRouter>
     )
